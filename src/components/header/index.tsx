4c8dd3a5ec0ad68c1fa925f8fc23d09832f46270
Aldovani <aldovanihcosta@gmail.com>
2024-06-11 11:07:23 -0300

    chore: fix header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,31 +3,33 @@ import { ActiveLink } from './active-link'
 
 export function Header() {
   return (
-    <header className="py-8 fixed bg-white w-full inset-0 flex justify-between container mx-auto">
-      <NavLink to="/">
-        <img src="/logo.svg" alt="" />
-      </NavLink>
-      <nav>
-        <ul className="flex  gap-x-6 lg:gap-x-[4.5rem]">
-          <li>
-            <ActiveLink to="/">Home</ActiveLink>
-          </li>
-          <li>
-            <ActiveLink to="/shop">Shop</ActiveLink>
-          </li>
-          <li>
-            <ActiveLink to="/about">About</ActiveLink>
-          </li>
-          <li>
-            <ActiveLink to="/contact">Contact</ActiveLink>
-          </li>
-        </ul>
-      </nav>
-      <div className="flex gap-8">
-        <NavLink to="/sing">
-          <img src="/user.svg" alt="" className="size-7" />
+    <header className="py-8  fixed bg-white w-full">
+      <div className="container mx-auto  flex  justify-between">
+        <NavLink to="/">
+          <img src="/logo.svg" alt="" />
         </NavLink>
-        <img src="/shopping-cart.svg" alt="" className="size-7" />
+        <nav>
+          <ul className="flex  gap-x-6 lg:gap-x-[4.5rem]">
+            <li>
+              <ActiveLink to="/">Home</ActiveLink>
+            </li>
+            <li>
+              <ActiveLink to="/shop">Shop</ActiveLink>
+            </li>
+            <li>
+              <ActiveLink to="/about">About</ActiveLink>
+            </li>
+            <li>
+              <ActiveLink to="/contact">Contact</ActiveLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex gap-8">
+          <NavLink to="/sing">
+            <img src="/user.svg" alt="" className="size-7" />
+          </NavLink>
+          <img src="/shopping-cart.svg" alt="" className="size-7" />
+        </div>
       </div>
     </header>
   )
