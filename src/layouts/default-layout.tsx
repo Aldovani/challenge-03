effae3654a8985b6b729d88eed28211cc8839f85
Aldovani <aldovanihcosta@gmail.com>
2024-06-11 15:11:17 -0300

    feat: add Header and footer to default layout

--- src/layouts/default-layout.tsx
+++ src/layouts/default-layout.tsx
@@ -1,5 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import { Footer } from '../components/footer'
+import { Header } from '../components/header'
+
 export function DefaultLayout() {
   return (
-    <div></div>
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
   )
 }
