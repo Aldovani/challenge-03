34159096cccd66aa556ca8ac31f6b23601f9f315
Aldovani <aldovanihcosta@gmail.com>
2024-06-12 17:33:05 -0300

    chore: add components to page

--- src/pages/shop.tsx
+++ src/pages/shop.tsx
@@ -1,13 +1,23 @@
 import { Banner } from '../components/banner'
 import { Benefits } from '../components/benefits'
+import { FilterBar } from '../components/filter-bar'
+import { Pagination } from '../components/pagination'
 import { ProductList } from '../components/product/product-list'
 
 export function ShopPage() {
   return (
     <main className=" w-full   pt-[6.25rem] ">
       <Banner page="Shop" />
-      <div className="container mx-auto">
+      <FilterBar />
+      <div className="mt-16 container mx-auto">
         <ProductList />
+        <Pagination
+          currentPage={2}
+          onChangePage={() => {}}
+          pageSize={16}
+          totalElements={40}
+          totalPages={5}
+        />
       </div>
       <Benefits />
     </main>
