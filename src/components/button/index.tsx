cdcbf0a1ed5b6302d40807d5d8e6b47072a92137
Aldovani <aldovanihcosta@gmail.com>
2024-06-13 15:25:52 -0300

    feat: add variants to component button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,15 +2,30 @@ import { ComponentProps, ReactNode } from 'react'
 import { tv, VariantProps } from 'tailwind-variants'
 
 const button = tv({
-  base: 'w-full bg-primary-500 text-white py-3 rounded-[0.3125rem] hover:bg-opacity-80',
+  base: 'w-full py-3 rounded-[0.3125rem] ',
+  variants: {
+    variants: {
+      outline:
+        'border  rounded-2xl hover:bg-primary-500 transition-colors hover:border-primary-500 hover:text-white',
+      primary: ' bg-primary-500 text-white  hover:bg-opacity-80',
+    },
+  },
+  defaultVariants: {
+    variants: 'primary',
+  },
 })
 
 type ButtonProps = { children: ReactNode } & ComponentProps<'button'> &
   VariantProps<typeof button>
 
-export function Button({ className, children, ...props }: ButtonProps) {
+export function Button({
+  className,
+  variants,
+  children,
+  ...props
+}: ButtonProps) {
   return (
-    <button className={button({ className })} {...props}>
+    <button className={button({ className, variants })} {...props}>
       {children}
     </button>
   )
