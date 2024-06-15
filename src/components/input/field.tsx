9cda8221df039c4e7f1c6900687fc80cebf5e83f
Aldovani <aldovanihcosta@gmail.com>
2024-06-15 17:48:04 -0300

    feat: add isError style to input

--- src/components/input/field.tsx
+++ src/components/input/field.tsx
@@ -3,13 +3,30 @@ import { tv, VariantProps } from 'tailwind-variants'
 
 const field = tv({
   base: 'w-full border border-gray-200 bg-white py-6 rounded-[0.625rem] placeholder:text-gray-200 px-8 outline-none',
+  variants: {
+    isError: { true: 'border-action-error' },
+  },
+  defaultVariants: {
+    isError: false,
+  },
 })
 
-type FieldProps = ComponentProps<'input'> & VariantProps<typeof field> 
+type FieldProps = ComponentProps<'input'> & VariantProps<typeof field>
 
-export const Field= forwardRef(({ className,...props }: FieldProps,ref:Ref<HTMLInputElement> | null) =>{
-  return <input ref={ref} {...props} className={field({ className })} {...props} />
-}
+export const Field = forwardRef(
+  (
+    { className, isError, ...props }: FieldProps,
+    ref: Ref<HTMLInputElement> | null,
+  ) => {
+    return (
+      <input
+        ref={ref}
+        {...props}
+        className={field({ className, isError })}
+        {...props}
+      />
+    )
+  },
 )
 
-Field.displayName="Field"
+Field.displayName = 'Field'
