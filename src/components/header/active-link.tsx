9a45b3e66d5fa5da8916975fb082f2e8fac89305
Aldovani <aldovanihcosta@gmail.com>
2024-06-16 15:42:10 -0300

    feat: create mobile menu

--- src/components/header/active-link.tsx
+++ src/components/header/active-link.tsx
@@ -2,14 +2,19 @@ import { NavLink, NavLinkProps } from 'react-router-dom'
 
 type ActiveLinkProps = NavLinkProps
 
-export function ActiveLink({ children, to, ...props }: ActiveLinkProps) {
+export function ActiveLink({
+  children,
+  to,
+  className,
+  ...props
+}: ActiveLinkProps) {
   return (
     <NavLink
       to={to}
       className={({ isActive }) =>
         isActive
-          ? 'text-primary-500 font-medium'
-          : 'text-black font-medium hover:text-primary-500 transition-colors'
+          ? `text-primary-500 font-medium ${className}`
+          : `text-black font-medium hover:text-primary-500 transition-colors ${className}`
       }
       {...props}
     >
