aea11c68c77b09a7f2c86ac1b0eabd0adcc428b7
Aldovani <aldovanihcosta@gmail.com>
2024-06-17 18:49:12 -0300

    fix: add spinner to button and input

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,5 +1,6 @@
 import { ComponentProps, ReactNode } from 'react'
 import { tv, VariantProps } from 'tailwind-variants'
+import { Spinner } from '../spinner'
 
 const button = tv({
   base: 'w-full py-3 rounded-[0.3125rem] ',
@@ -8,6 +9,9 @@ const button = tv({
       outline:
         'border  rounded-2xl hover:bg-primary-500 transition-colors hover:border-primary-500 hover:text-white',
       primary: ' bg-primary-500 text-white  hover:bg-opacity-80',
+    },
+    isLoading: {
+      true: 'text-white gap-2 bg-primary-500 flex items-center justify-center border-primary-500',
     },
   },
   defaultVariants: {
@@ -22,11 +26,17 @@ export function Button({
   className,
   variants,
   children,
+  isLoading,
   ...props
 }: ButtonProps) {
   return (
-    <button className={button({ className, variants })} {...props}>
-      {children}
+    <button className={button({ className, variants, isLoading })} {...props}>
+      {!isLoading && children}
+      {isLoading && (
+        <>
+          <Spinner colors="white" /> <span>loading...</span>
+        </>
+      )}
     </button>
   )
 }
--- src/components/input/field.tsx
+++ src/components/input/field.tsx
@@ -1,30 +1,38 @@
 import { ComponentProps, forwardRef, Ref } from 'react'
 import { tv, VariantProps } from 'tailwind-variants'
+import { Spinner } from '../spinner'
 
 const field = tv({
-  base: 'w-full border border-gray-200 bg-white py-6 rounded-[0.625rem] placeholder:text-gray-200 px-8 outline-none',
+  base: 'w-full border border-gray-200 bg-white py-6 rounded-[0.625rem] placeholder:text-gray-200 px-8 outline-none disabled:opacity-50',
   variants: {
     isError: { true: 'border-action-error' },
   },
   defaultVariants: {
     isError: false,
+    isLoading: false,
   },
 })
 
-type FieldProps = ComponentProps<'input'> & VariantProps<typeof field>
+type FieldProps = ComponentProps<'input'> &
+  VariantProps<typeof field> & {
+    isLoading?: boolean
+  }
 
 export const Field = forwardRef(
   (
-    { className, isError, ...props }: FieldProps,
+    { className, isLoading = false, isError, ...props }: FieldProps,
     ref: Ref<HTMLInputElement> | null,
   ) => {
     return (
-      <input
-        ref={ref}
-        {...props}
-        className={field({ className, isError })}
-        {...props}
-      />
+      <div className="relative">
+        <input
+          ref={ref}
+          {...props}
+          className={field({ className, isError })}
+          {...props}
+        />
+        {isLoading && <Spinner className="absolute right-4 top-1/2" />}
+      </div>
     )
   },
 )
