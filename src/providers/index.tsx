0d652fe54f5ed1ecd4c495ca2147f6e0b6e90cf6
Aldovani <aldovanihcosta@gmail.com>
2024-06-17 19:50:16 -0300

    fix: remove router from provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,16 +1,16 @@
-import { RouterProvider } from 'react-router-dom'
 import { AuthProvider } from './auth-provider'
 import { Provider as ReduxProvider } from 'react-redux'
 
-import { router } from '../routes'
 import { store } from '../stores'
 
-export function Providers() {
+type ProvidersProps = {
+  children?: React.ReactNode
+}
+
+export function Providers({ children }: ProvidersProps) {
   return (
     <ReduxProvider store={store}>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <AuthProvider>{children}</AuthProvider>
     </ReduxProvider>
   )
 }
