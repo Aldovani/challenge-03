2f254c51a417858c9978548ead41a0fd59e7a760
Aldovani <aldovanihcosta@gmail.com>
2024-06-17 00:11:30 -0300

    fix: add close when click outside the component

--- src/components/cart/index.tsx
+++ src/components/cart/index.tsx
@@ -11,7 +11,9 @@ type CartProps = {
 }
 
 export function Cart({ isOpen, isOpening, onClose }: CartProps) {
-  const { handleClose, isClosing, containerRef } = useCart({ onClose })
+  const { handleClose, isClosing, containerRef } = useCart({
+    onClose,
+  })
 
   return (
     <ReactPortal containerId="cart-container">
@@ -20,12 +22,12 @@ export function Cart({ isOpen, isOpening, onClose }: CartProps) {
           data-open={isOpening}
           className="data-[open='true']:scale-x-100 data-[open='true']:translate-x-0  duration-200 fixed  grid grid-cols-[1fr_417px] w-full h-full bg-black bg-opacity-20 z-[100] inset-0"
         >
-          <div className=""></div>
+          <div id="overlay" onClick={() => handleClose()}></div>
           <div
             data-open={isOpening}
             data-close={isClosing}
             ref={containerRef}
-            className="data-[close='true']:animate-close-cart bg-white h-full grid grid-rows-[64px_1fr_auto] translate-x-full animate-open-cart"
+            className="data-[close='true']:animate-close-cart bg-white h-full max-h-screen grid grid-rows-[64px_1fr_168px] translate-x-full animate-open-cart"
           >
             <div>
               <header className="flex items-center justify-between pr-10 pt-7 pl-8">
@@ -35,9 +37,10 @@ export function Cart({ isOpen, isOpening, onClose }: CartProps) {
                 </button>
               </header>
               <div className="px-16 pl-8 pr-[100px] mt-6">
-                <div className="  h-[1px] bg-[#d4d4d4]  " />
+                <div className=" h-[1px] bg-[#d4d4d4]  " />
               </div>
             </div>
+
             <CartList />
 
             <CartFooter onCloseCart={handleClose} />
