7a8d155b63c097c78ad5812aa854ce34596c60ad
Aldovani <aldovanihcosta@gmail.com>
2024-06-18 00:32:16 -0300

    fix: warring index

--- src/components/product/loading/index.tsx
+++ src/components/product/loading/index.tsx
@@ -7,12 +7,14 @@ type ProductListLoadingProps = {
 export function ProductListLoading({
   numberOfElements = 8,
 }: ProductListLoadingProps) {
-  const arrayOfNumberOfElements = new Array(numberOfElements).fill(() => '')
+  const arrayOfNumberOfElements = new Array(numberOfElements).fill(() =>
+    Math.random().toString(),
+  )
 
   return (
     <section className="grid px-8 sm:px-0 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-8 gap-y-10">
-      {arrayOfNumberOfElements.map((item) => (
-        <ProductListItemLoading key={item} />
+      {arrayOfNumberOfElements.map((_, indx) => (
+        <ProductListItemLoading key={indx} />
       ))}
     </section>
   )
