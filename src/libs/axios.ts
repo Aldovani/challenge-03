06e319b087741084fe004ef9c99f79780205c566
Aldovani <aldovanihcosta@gmail.com>
2024-06-17 22:42:42 -0300

    feat: create axios instance to fetch products

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,3 +1,7 @@
 import axios from 'axios'
 
 export const cepApi = axios.create({ baseURL: 'https://viacep.com.br/ws/' })
+
+export const productsApi = axios.create({
+  baseURL: 'http://localhost:3333/products',
+})
