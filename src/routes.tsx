0c1c6732c5cc9d20f9c8c1be67afb59ee50073dc
Aldovani <aldovanihcosta@gmail.com>
2024-06-19 12:24:59 -0300

    feat: add not found page

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,13 +3,14 @@ import { DefaultLayout } from './layouts/default-layout'
 
 import { HomePage } from './pages/home'
 import { ShopPage } from './pages/shop'
-import { ProductDetailsPage } from './pages/shop/name'
+import { ProductDetailsPage } from './pages/shop/id'
 import { ContactPage } from './pages/contact/'
 import { CartPage } from './pages/cart'
 import { AuthLayout } from './layouts/auth-layout'
 import { SignInPage } from './pages/auth/sign-in'
 import { CheckoutPage } from './pages/checkout/'
 import { CheckoutSuccessPage } from './pages/checkout/success'
+import { NotFoundPage } from './pages/not-found'
 
 export function Router() {
   return (
@@ -17,16 +18,17 @@ export function Router() {
       <Route element={<DefaultLayout />}>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
-        <Route path="/shop/:name" element={<ProductDetailsPage />} />
+        <Route path="/shop/:id" element={<ProductDetailsPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/check-out" element={<CheckoutPage />} />
       </Route>
       <Route element={<AuthLayout />}>
-        <Route path="/auth/sing-in" element={<SignInPage />} />
+        <Route path="/auth/sign-in" element={<SignInPage />} />
       </Route>
 
       <Route path="/check-out/success" element={<CheckoutSuccessPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
 }
