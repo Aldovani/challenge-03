ef46e9f0c726d35b3387d8562a7d49ec380e0c86
Aldovani <aldovanihcosta@gmail.com>
2024-06-19 12:29:54 -0300

    feat fetch data

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,23 +1,28 @@
+import { useSelector } from 'react-redux'
 import { Banner } from '../../components/banner'
 import { Benefits } from '../../components/benefits'
 import { FilterBar } from '../../components/filter-bar'
 import { Pagination } from '../../components/pagination'
 import { ProductList } from '../../components/product/product-list'
+import { IState } from '../../stores'
+import { IProduct } from '../../stores/modules/products/types'
 
 export function ShopPage() {
+  const products = useSelector<IState, IProduct[]>(
+    (state) => state.products.items,
+  )
+
+  const loading = useSelector<IState, boolean>(
+    (state) => state.products.loading,
+  )
+
   return (
     <main className=" w-full   pt-[6.25rem] ">
       <Banner page="Shop" />
       <FilterBar />
       <div className="mt-16 container mx-auto">
-        <ProductList />
-        <Pagination
-          currentPage={2}
-          onChangePage={() => {}}
-          pageSize={16}
-          totalElements={40}
-          totalPages={5}
-        />
+        <ProductList items={products} isLoading={loading} />
+        <Pagination />
       </div>
       <Benefits />
     </main>
