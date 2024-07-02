6f66d062634dec579e50706cbbefc43b88e44ab0
Aldovani <aldovanihcosta@gmail.com>
2024-07-01 23:45:12 -0300

    chore: redesign login page

--- src/layouts/auth-layout.tsx
+++ src/layouts/auth-layout.tsx
@@ -1,18 +1,19 @@
 import { Outlet } from 'react-router-dom'
-import { IMAGES } from '../assets/images'
+import AuthLayoutBG from '../assets/auth-layout.png'
 
 export function AuthLayout() {
   return (
     <>
       <div
         data-testid="auth-layout"
-        className="grid lg:grid-cols-[60%_40%] overflow-y-hidden min-h-screen "
+        className="grid lg:grid-cols-[50%_50%] overflow-y-hidden min-h-screen "
       >
+        <Outlet />
+
         <div
-          style={{ backgroundImage: `url(${IMAGES['auth-background']})` }}
+          style={{ backgroundImage: `url(${AuthLayoutBG})` }}
           className="hidden lg:block h-full bg-[100%] bg-cover"
         ></div>
-        <Outlet />
       </div>
     </>
   )
--- src/pages/auth/sign-in/index.tsx
+++ src/pages/auth/sign-in/index.tsx
@@ -2,7 +2,9 @@ import { Link } from 'react-router-dom'
 import { Button } from '../../../components/button'
 import { Input } from '../../../components/input'
 import { useSignIn } from './use-sign-in'
-import { ICONS } from '../../../assets/icons'
+import GreenLogo from '../../../assets/logo-green.svg'
+import FacebookIcon from '../../../assets/facebook-icon.svg'
+import GoogleIcon from '../../../assets/google-icon.svg'
 
 export function SignInPage() {
   const {
@@ -17,52 +19,99 @@ export function SignInPage() {
   return (
     <main className="flex flex-col py-8  sm:py-10 px-8 sm:px-20">
       <Link to="/">
-        <img src={ICONS.logo} alt="Logo Furniro" className="w-32" />
+        <img src={GreenLogo} alt="Logo Furniro" className="w-32" />
       </Link>
 
-      <div className="mt-10">
-        <h1 className="sm:text-64 text-4xl font-eb-garamond font-bold  text-gray-500">
-          Welcome back
-        </h1>
-        <p className="mt-2 text-gray-200 max-w-96  ">
-          Browse our wide selection of comfortable and stylish sofas.
-        </p>
-      </div>
+      <div className="xl:px-16 mt-10">
+        <header className="mt-10">
+          <h1 className="sm:text-64 text-4xl font-lato font-medium italic text-emerald-700 max-w-[250px] leading-[120%] ">
+            Welcome back to Furniro Login
+          </h1>
+          <p className="mt-2 text-zinc-400  ">
+            Vorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </header>
 
-      <form
-        className="mt-12"
-        onSubmit={handleSubmit((data) => handleSignInWithEmail(data.email))}
-      >
-        <Input.Label name="Email" htmlFor="email">
-          <Input.Field
-            id="email"
-            placeholder="abc@exemple.com"
-            className="mt-5"
-            isError={!!errors.email}
-            type="email"
-            {...register('email')}
-          />
-          <Input.MessageError error={errors.email?.message} />
-        </Input.Label>
+        <div className="flex flex-col gap-4 mt-8">
+          <Button
+            variants="outline"
+            className="flex gap-3 py-4  justify-center "
+            onClick={handleSignInWithGoogle}
+          >
+            <img src={GoogleIcon} alt="google icon" /> Sign-in with Google
+          </Button>
+          <Button
+            variants="outline"
+            className="flex gap-3 py-4  justify-center"
+            onClick={handleSignInWithFacebook}
+          >
+            <img src={FacebookIcon} alt="facebook icon" /> Sign-in with Facebook
+          </Button>
+        </div>
 
-        <Button type="submit" className="mt-4">
-          Sign-in
-        </Button>
-      </form>
+        <div className="flex gap-3 items-center mt-6 ">
+          <span className="h-[1px] w-full block bg-zinc-200"> </span>
+          <span className="text-zinc-400">Or </span>
+          <span className="h-[1px] w-full bg-zinc-200 block"> </span>
+        </div>
 
-      <div className="flex gap-3 items-center mt-6 ">
-        <span className="h-[1px] w-full block bg-gray-100"> </span>
-        <span className="text-gray-200">Or </span>
-        <span className="h-[1px] w-full bg-gray-100 block"> </span>
-      </div>
+        <form
+          className="mt-8"
+          onSubmit={handleSubmit((data) => handleSignInWithEmail(data.email))}
+        >
+          <Input.Label name="Email" htmlFor="email">
+            <Input.Field
+              id="email"
+              className="mt-2"
+              placeholder="email@exempla.com"
+              isError={!!errors.email}
+              type="email"
+              {...register('email')}
+            />
+            <Input.MessageError error={errors.email?.message} />
+          </Input.Label>
+          <Input.Label name="Password" htmlFor="email" className="mt-4">
+            <Input.Field
+              id="email"
+              className="mt-2"
+              placeholder="••••••••••••"
+              isError={!!errors.email}
+              type="email"
+              {...register('email')}
+            />
+            <Input.MessageError error={errors.email?.message} />
+          </Input.Label>
 
-      <div className="flex flex-col gap-4 mt-8">
-        <Button variants="outline" onClick={handleSignInWithGoogle}>
-          Sign-in with Google
-        </Button>
-        <Button variants="outline" onClick={handleSignInWithFacebook}>
-          Sign-in with Facebook
-        </Button>
+          <div className="flex justify-between items-center mt-6 max-sm:flex-col max-sm:gap-2">
+            <div className=" flex items-center gap-4">
+              <input
+                type="checkbox"
+                value="Remember"
+                id="Remember"
+                className="peer opacity-0 invisible w-0 h-0 absolute"
+              />
+              <span className="size-5 peer-checked:bg-emerald-700  peer-checked:border-emerald-700 border border-gray-200  relative after:block after:size-2 after:top-1/2 after:left-1/2 after:rounded-sm after:bg-white after:translate-x-1/2 after:translate-y-1/2"></span>
+
+              <label
+                className="peer-checked:text-emerald-700   peer-checked:font-normal text-zinc-400  font-medium"
+                htmlFor="Remember"
+              >
+                Remember me
+              </label>
+            </div>
+
+            <Link
+              className="text-zinc-400 hover:text-emerald-600"
+              to="/forget-password"
+            >
+              Forgot password?
+            </Link>
+          </div>
+
+          <Button type="submit" className="mt-8">
+            Sign-in
+          </Button>
+        </form>
       </div>
     </main>
   )
