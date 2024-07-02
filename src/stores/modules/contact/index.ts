337d95079a9a8e1beed609449be603feb636d139
Aldovani <aldovanihcosta@gmail.com>
2024-07-01 22:15:09 -0300

    feat: contact with redux to redux-toolkit

---
+++ src/stores/modules/contact/index.ts
@@ -0,0 +1,19 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { IContact, IContactState } from './types'
+
+const INITIAL_STATE: IContactState = {
+  contacts: [],
+}
+
+const contactSlice = createSlice({
+  name: 'contact',
+  initialState: INITIAL_STATE,
+  reducers: {
+    saveContact(state, action: PayloadAction<IContact>) {
+      state.contacts.push(action.payload)
+    },
+  },
+})
+
+export const contact = contactSlice.reducer
+export const { saveContact } = contactSlice.actions
