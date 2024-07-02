409bdd21c0a8a03af287a24212350360a352a282
Aldovani <aldovanihcosta@gmail.com>
2024-07-01 22:19:46 -0300

    fix: remove productApi

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,3 @@
 import axios from 'axios'
 
 export const cepApi = axios.create({ baseURL: 'https://viacep.com.br/ws/' })
-
-export const productsApi = axios.create({
-  baseURL: 'http://localhost:3333/products',
-})
